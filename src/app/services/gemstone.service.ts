42acb6afa0c380f0938d96672192403e7e95db49
Vitor Eble <vitoreble06@gmail.com>
2025-05-22 23:07:27 -0300

    teste da api 01

--- src/app/services/gemstone.service.ts
+++ src/app/services/gemstone.service.ts
@@ -2,12 +2,14 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 
+import { environment } from '../../environments/environment';
+
 @Injectable({
   providedIn: 'root'
 })
 export class GemstoneService {
 
-  private apiUrl = '/api/skyblock/bazaar';
+  private apiUrl = environment.apiUrl;
 
   constructor(private http: HttpClient) { }
 
